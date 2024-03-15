7f5f9f55bed91278971d25108ed078de13f3773b
Nick-552 <TTchip552@ya.ru>
2024-03-16 00:30:58 +0300

    made the combination of chat_id and link_id UNIQUE

--- migrations/chats_links_scheme.sql
+++ migrations/chats_links_scheme.sql
@@ -6,5 +6,6 @@ CREATE TABLE IF NOT EXISTS chats_links
     id              BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
     link_id         BIGINT           REFERENCES links (id) NOT NULL,
     chat_id         BIGINT           REFERENCES chats (id) NOT NULL
+    CONSTRAINT unique_chats_links UNIQUE CHECK (link_id <> chat_id)
 )
 --rollback drop table chats_links
